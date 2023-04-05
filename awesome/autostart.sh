c516178c1c4658060b0b81f09e558cc66bb5427a
gaiatuven <ggwiese@gmail.com>
2023-04-05 13:13:49 +0200

    Config files for awesomeWM

---
+++ awesome/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run nm-applet
+#run xfce4-power-manager
+#run blueberry-tray
+#run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run volumeicon
+run nitrogen --restore
+#run applications from startup
+#run firefox
+run torguard 
